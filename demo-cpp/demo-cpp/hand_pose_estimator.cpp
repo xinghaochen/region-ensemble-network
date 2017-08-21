b418e9b54b7d1ad383e9022f840dd7f0711cd664
Xinghao Chen <chenxh09thu@gmail.com>
2017-08-21 09:40:50 +0800

    update pydemo to new pyrealsense api & add cpp demo

---
+++ demo-cpp/demo-cpp/hand_pose_estimator.cpp
@@ -0,0 +1,214 @@
+/*************************************************************************
+> File Name: output_pose.cpp
+> Author: Guo Hengkai
+> Description:
+> Created Time: Sun 06 Nov 2016 10:47:29 AM CST
+************************************************************************/
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#include "boost/algorithm/string.hpp"
+#include "google/protobuf/text_format.h"
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/net.hpp"
+#include "caffe/proto/caffe.pb.h"
+#include "caffe/util/format.hpp"
+#include "caffe/util/io.hpp"
+
+#include "hand_pose_estimator.h"
+
+
+using caffe::Blob;
+using caffe::Caffe;
+using caffe::Datum;
+using caffe::Net;
+using std::string;
+using std::vector;
+
+HandPoseEstimator::HandPoseEstimator() {
+	gpu = -1;	// cpu only
+	fx = 463.889;
+	fy = 463.889;
+	ux = 320;
+	uy = 240;
+	lower = 1;
+	upper = 650;
+	cube_length.push_back(150);
+	cube_length.push_back(150);
+	cube_length.push_back(150);
+	height = 96;
+	width = 96;
+	output_blob = "predict";
+	weights = "D:/Projects/region-ensemble-network/models/model_icvl_ren_9x6x6.caffemodel";
+	model = "D:/Projects/region-ensemble-network/models/deploy_icvl_ren_9x6x6.prototxt";
+	init_model();
+}
+
+HandPoseEstimator::~HandPoseEstimator() {
+
+}
+
+int HandPoseEstimator::init_model() {
+	// initialize model
+	if (gpu < 0) {
+		LOG(INFO) << "Using CPU";
+		Caffe::set_mode(Caffe::CPU);
+	}
+	else {
+		LOG(INFO) << "Using GPU " << gpu;
+		Caffe::SetDevice(gpu);
+		Caffe::set_mode(Caffe::GPU);
+	}
+	test_net = boost::shared_ptr<Net<float> >(new Net<float>(model, caffe::TEST));
+	test_net->CopyTrainedLayersFrom(weights);
+	return 1;
+}
+
+vector<float> HandPoseEstimator::predict(const cv::Mat& cv_img) {
+
+	// get outputs and save
+	const boost::shared_ptr<Blob<float> > blob =
+		test_net->blob_by_name(output_blob);
+	int batch_size = blob->num();
+	int res_size = blob->count() / batch_size;
+	LOG(INFO) << "batch size: " << batch_size;
+	// pre processing
+	vector<float> center;
+	//cv::imshow("cv_img", cv_img);
+
+	get_center(cv_img, center, lower, upper);
+	cv::Mat crop = crop_image(cv_img, center, cube_length, fx, fy, height, width);
+	//cv::imshow("crop", crop);
+
+	//feed data
+	boost::shared_ptr<Blob<float> > blob_data = test_net->blob_by_name("data");
+	Mat2Blob(crop, blob_data);
+	// forward
+	test_net->Forward();
+	const float* data = blob->cpu_data();
+
+	vector<float> result;
+	float c1 = center[0];
+	float c2 = center[1];
+	float c3 = center[2];
+	std::cout << c1 << " " << c2 << " " << c3 << std::endl;
+
+	for (int k = 0; k < res_size/3; ++k) {
+		float u = *data;
+		++data;
+		float v = *data;
+		++data;
+		float d = *data;
+		++data;
+		// transform
+		u = u * cube_length[0] * fabs(fx) / c3 + c1;
+		v = v * cube_length[1] * fabs(fy) / c3 + c2;
+		d = d * cube_length[2] + c3;
+		//std::cout << u << " " << v << " " << d << std::endl;
+
+		result.push_back(u);
+		result.push_back(v);
+		result.push_back(d);
+	}
+
+	return result;
+}
+
+void HandPoseEstimator::Mat2Blob(const cv::Mat &mat,
+	boost::shared_ptr<Blob<float> > blob)
+{
+	assert(!mat.empty());
+	float* blob_data = blob->mutable_cpu_data();
+
+	int height = mat.rows;
+	int width = mat.cols;
+	int channel = 1;
+	int top_index = 0;
+
+	for (int c = 0; c < channel; ++c)
+	{
+		// cout << mat[c] << endl;
+		for (int h = 0; h < height; ++h)
+		{
+			const float* ptr = mat.ptr<float>(h);
+			for (int w = 0; w < width; ++w)
+			{
+				// int top_index = (c * height + h) * width + w;
+				blob_data[top_index++] = ptr[w];
+			}
+		}
+	}
+}
+
+void HandPoseEstimator::get_center(const cv::Mat& cv_img, vector<float>& center, int lower, int upper) {
+	// TODO(guohengkai): remove the hard threshold if necessary 0 ~ 880
+	center = vector<float>(3, 0);
+	int count = 0;
+	int min_val = INT_MAX;
+	int max_val = INT_MIN;
+	for (int r = 0; r < cv_img.rows; ++r) {
+		const float* ptr = cv_img.ptr<float>(r);
+		for (int c = 0; c < cv_img.cols; ++c) {
+			//std::cout << ptr[c] << std::endl;
+			if (ptr[c] <= upper && ptr[c] >= lower) {
+				center[0] += c;
+				center[1] += r;
+				center[2] += ptr[c];
+				++count;
+			}
+			/*
+			if (int(ptr[c]) > 0)
+			min_val = std::min(int(ptr[c]), min_val);
+			max_val = std::max(int(ptr[c]), max_val);
+			*/
+		}
+	}
+	if (count) {
+		for (int i = 0; i < 3; ++i) {
+			center[i] /= count;
+		}
+		//std::cout << center[0] << " " << center[1] << " " << center[2] << std::endl;
+	}
+	else
+	{
+		center.clear();
+		// LOG(INFO) << "max: " << max_val << ", min: " << min_val;
+	}
+}
+
+cv::Mat HandPoseEstimator::crop_image(const cv::Mat& cv_img,
+	const vector<float>& center, const vector<int>& cube_length,
+	float fx, float fy, int height, int width) {
+	float xstart = center[0] - cube_length[0] / center[2] * fabs(fx);
+	float xend = center[0] + cube_length[0] / center[2] * fabs(fx);
+	float ystart = center[1] - cube_length[1] / center[2] * fabs(fy);
+	float yend = center[1] + cube_length[1] / center[2] * fabs(fy);
+	float xscale = 2.0 / (xend - xstart);
+	float yscale = 2.0 / (yend - ystart);
+	//std::cout << "crop:" << xstart << " " << xend << " " << ystart << " " << yend << std::endl;
+	//std::cout << "cube:" << cube_length[0] << " " << cube_length[1] << std::endl;
+
+	vector<cv::Point2f> src, dst;
+	src.push_back(cv::Point2f(xstart, ystart));
+	dst.push_back(cv::Point2f(0, 0));
+	src.push_back(cv::Point2f(xstart, yend));
+	dst.push_back(cv::Point2f(0, height - 1));
+	src.push_back(cv::Point2f(xend, ystart));
+	dst.push_back(cv::Point2f(width - 1, 0));
+	cv::Mat trans = cv::getAffineTransform(src, dst);
+	cv::Mat res_img;
+	cv::warpAffine(cv_img, res_img, trans, cv::Size(width, height),
+		cv::INTER_LINEAR, cv::BORDER_CONSTANT, center[2] + cube_length[2]);
+	res_img -= center[2];
+	res_img = cv::max(res_img, -cube_length[2]);
+	res_img = cv::min(res_img, cube_length[2]);
+	res_img /= cube_length[2];
+	return res_img;
+}
---
+++ demo-cpp/demo-cpp/run_hand_pose.cpp
@@ -0,0 +1,106 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2015 Intel Corporation. All Rights Reserved.
+
+/////////////////////////////////////////////////////
+// librealsense tutorial #1 - Accessing depth data //
+/////////////////////////////////////////////////////
+
+// First include the librealsense C++ header file
+#include <librealsense/rs.hpp>
+#include <cstdio>
+#include <opencv2\opencv.hpp>
+#include "hand_pose_estimator.h"
+
+int main() try
+{
+	// Create a context object. This object owns the handles to all connected realsense devices.
+	rs::context ctx;
+	printf("There are %d connected RealSense devices.\n", ctx.get_device_count());
+	if (ctx.get_device_count() == 0) return EXIT_FAILURE;
+
+	// This tutorial will access only a single device, but it is trivial to extend to multiple devices
+	rs::device * dev = ctx.get_device(0);
+	printf("\nUsing device 0, an %s\n", dev->get_name());
+	printf("    Serial number: %s\n", dev->get_serial());
+	printf("    Firmware version: %s\n", dev->get_firmware_version());
+
+	// Configure depth to run at VGA resolution at 30 frames per second
+	dev->enable_stream(rs::stream::depth, 640, 480, rs::format::z16, 30);
+
+	dev->start();
+
+	// Determine depth value corresponding to one meter
+	const uint16_t one_meter = static_cast<uint16_t>(1.0f / dev->get_depth_scale());
+
+	// caffe model
+	HandPoseEstimator hpe;
+
+	cv::Mat depth(480, 640, CV_32FC1);
+	while (true)
+	{
+		// This call waits until a new coherent set of frames is available on a device
+		// Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+		dev->wait_for_frames();
+
+		// Retrieve depth data, which was previously configured as a 640 x 480 image of 16-bit depth values
+		const uint16_t * depth_frame = reinterpret_cast<const uint16_t *>(dev->get_frame_data(rs::stream::depth));
+		const uint16_t one_meter = static_cast<uint16_t>(1.0f / dev->get_depth_scale());
+
+		// Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and and approximating the coverage of pixels within one meter
+		for (int y = 0; y<480; ++y)
+		{
+			for (int x = 0; x<640; ++x)
+			{
+				float depth_val = *depth_frame++;
+				if (depth_val > 0) {
+					depth.at<float>(cv::Point(x, y)) = depth_val * dev->get_depth_scale() * 1000;
+					//std::cout << depth_val * dev->get_depth_scale() * 1000 << std::endl;
+				}
+				else
+					depth.at<float>(cv::Point(x, y)) = 10000;
+			}
+		}
+		//std::cout << dev->get_depth_scale() << std::endl;
+		cv::Mat dst;
+		cv::flip(depth, dst, 1);
+		//cv::imshow("dst", dst);
+		vector<float> result = hpe.predict(dst);
+		// show
+		//for (int idx = 0; idx < result.size(); idx++)
+		//	std::cout << result[idx] << std::endl;
+		cv::Mat show(dst.clone());
+		show.setTo(10000, show == 0);
+		cv::threshold(show, show, 1000, 1000, cv::THRESH_TRUNC);
+
+		double minVal, maxVal;
+		cv::minMaxLoc(show, &minVal, &maxVal);
+		show.convertTo(show, CV_8U, 255.0 / (maxVal - minVal), -minVal * 255.0 / (maxVal - minVal));
+
+		cv::cvtColor(show, show, CV_GRAY2BGR);
+		for (int i = 0; i < result.size()/3; ++i) {
+			cv::circle(show, cv::Point2f(result[i*3], result[i*3+1]), 8,
+				cv::Scalar(0, 0, 255), -1);
+		}
+		int joint_id_start[] = { 0, 1, 2, 0, 4, 5, 0, 7, 8, 0, 10, 11, 0, 13, 14 };
+		int  joint_id_end[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+		// draw bone
+		for (int i = 0; i < 15; i++) {
+			int k1 = joint_id_start[i];
+			int k2 = joint_id_end[i];
+			cv::line(show, cv::Point2f(result[k1 * 3], result[k1 * 3 + 1]),
+				cv::Point2f(result[k2 * 3], result[k2 * 3 + 1]),
+				cv::Scalar(0, 255, 0), 3);
+		}
+		cv::imshow("depth", show);
+		cv::waitKey(1);
+	}
+
+	return EXIT_SUCCESS;
+}
+catch (const rs::error & e)
+{
+	// Method calls against librealsense objects may throw exceptions of type rs::error
+	printf("rs::error was thrown when calling %s(%s):\n", e.get_failed_function().c_str(), e.get_failed_args().c_str());
+	printf("    %s\n", e.what());
+	return EXIT_FAILURE;
+}
